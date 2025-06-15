7c4f7488821a9b53dbc76e5e70c045cddf0f7ece
Daan Hautekeete <Hautekeete.Daan@gmail.com>
2025-06-15 11:16:51 +0200

    Examen Juni - JSON
    - Maken oefening leerlingen

---
+++ Voorbereiding examen juni/Json/Oefening leerlingen/JSON_Leerlingen_examen/Form1.cs
@@ -0,0 +1,32 @@
+﻿using JSON_Leerlingen_examen.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JSON_Leerlingen_examen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //data inladen
+            Leerlingen leerlingen = new Leerlingen();
+
+            foreach(Leerlingen leerling in leerlingen.lijstLeerlingen)
+            {
+                listBox1.Items.Add(leerling);
+            }
+        }
+    }
+}
---
+++ Voorbereiding examen juni/Json/Oefening leerlingen/JSON_Leerlingen_examen/Model/Leerlingen.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace JSON_Leerlingen_examen.Model
+{
+    public class Leerlingen
+    {
+        //fields aanmaken
+        private string _id;
+        private string _naam;
+        private string _geboortedatum;
+        private string _klas;
+        private string _email;
+
+        private List<Leerlingen> _lijstLeerlingen;
+
+        //properties aanmaken
+        public string Id { get { return _id; } set { _id = value; } }
+        public string Naam { get { return _naam; } set { _naam = value; } }
+        public string Geboortedatum { get { return _geboortedatum; } set { _geboortedatum = value; } }
+        public string Klas { get { return _klas; } set { _klas = value; } }
+        public string Email { get { return _email; } set { _email = value; } }
+
+        public List<Leerlingen> lijstLeerlingen
+        {
+            get
+            {
+                string json = File.ReadAllText("leerlingen.json");
+                _lijstLeerlingen = JsonConvert.DeserializeObject<List<Leerlingen>>(json);
+                return _lijstLeerlingen;
+            }
+            set { _lijstLeerlingen = value; }
+        }
+
+        public override string ToString()
+        {
+            return this.Naam + " - " + this.Klas + " - " + this.Email + " - " + this.Geboortedatum + " - leeftijd: " + this.BerekenLeeftijd();
+        }
+
+        public int BerekenLeeftijd()
+        {
+            DateTime geboortedatum = DateTime.Parse(this.Geboortedatum);
+            DateTime nu = DateTime.Now;
+            int leeftijd = nu.Year - geboortedatum.Year;
+            if (geboortedatum.Date > nu.AddYears(-leeftijd)) { leeftijd--; };
+            return leeftijd;
+        }
+    }
+}
